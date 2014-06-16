50f2de5e674b4b825131844067f405241103e015
swansong86 <mccormickrobbie@gmail.com>
2014-06-16 13:48:18 +1000

    added a test class

---
+++ RMTest.h
@@ -0,0 +1,13 @@
+//
+//  RMTest.h
+//  Git Test
+//
+//  Created by Rob Mccormick on 16/06/2014.
+//  Copyright (c) 2014 Rob Mccormick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMTest : NSObject
+
+@end
---
+++ RMTest.m
@@ -0,0 +1,13 @@
+//
+//  RMTest.m
+//  Git Test
+//
+//  Created by Rob Mccormick on 16/06/2014.
+//  Copyright (c) 2014 Rob Mccormick. All rights reserved.
+//
+
+#import "RMTest.h"
+
+@implementation RMTest
+
+@end
